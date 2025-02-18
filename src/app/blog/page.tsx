49ccbef976aa29e833bcfcc1a96ea0ea73f8c70b
pawaaan9 <pawankanchana99@gmail.com>
2025-02-18 17:47:58 +0530

    ourwork

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
-import About from "@/components/pages/about";
 
 export default function Home() {
   return (
